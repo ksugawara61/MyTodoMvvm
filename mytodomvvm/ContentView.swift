a45a9175aeedecd5f04317462ab0c870a0f49564
ksugawara61 <katsuya@kokohadoko.net>
2020-05-17 16:22:46 +0900

    Initial Commit

---
+++ mytodomvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mytodomvvm
+//
+//  Created by 菅原勝也 on 2020/05/17.
+//  Copyright © 2020 菅原勝也. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mytodomvvmTests/mytodomvvmTests.swift
@@ -0,0 +1,34 @@
+//
+//  mytodomvvmTests.swift
+//  mytodomvvmTests
+//
+//  Created by 菅原勝也 on 2020/05/17.
+//  Copyright © 2020 菅原勝也. All rights reserved.
+//
+
+import XCTest
+@testable import mytodomvvm
+
+class mytodomvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
